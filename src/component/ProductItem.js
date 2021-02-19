00fff8b71d8c6f120add21d9560be7a1d5d4dc70
Atabaev <blackpanda7@mail.ru>
2021-02-19 18:53:17 +0600

    console item

--- src/component/ProductItem.js
+++ src/component/ProductItem.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import image from '../img/Clock.jpg'
 
-const ProductItem = (props) => {
+const ProductItem =(props)=> {
+    
     return (
         <div className="card">
             <img className="card-img-top" src={image} alt="Card cap"/>
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.description}</p>
-                    <button href="#" class="btn btn-primary">Go somewhere</button>
+                    <button onClick={props.consoleItem} href="#" className="btn btn-primary"><i className="fa fa-shopping-cart"></i></button>
                 </div>
         </div>
     );
--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 import ProductItem from './ProductItem';
 
-const ProductList = () => {
-    return (
-        <ul className='row product-list'>
-            <li className="col-3"><ProductItem title="card 1" description="1"/></li>
-            <li className="col-3"><ProductItem title="card 2" description="2"/></li>
-            <li className="col-3"><ProductItem title="card 3" description="3"/></li>
-            <li className="col-3"><ProductItem title="card 4" description="4"/></li>
-        </ul>
-    );
+class ProductList extends Component {
+    // return (
+    //     <ul className='row product-list'>
+    //         <li className="col-3"><ProductItem title="card 1" description="1"/></li>
+    //         <li className="col-3"><ProductItem title="card 2" description="2"/></li>
+    //         <li className="col-3"><ProductItem title="card 3" description="3"/></li>
+    //         <li className="col-3"><ProductItem title="card 4" description="4"/></li>
+    //     </ul>
+    // );
+    state = {
+        prodlist: [
+            {id: 10, title:"card 1", description:"1" },
+            {id: 20, title:"card 2", description:"2" },
+            {id: 30, title:"card 3", description:"3" },
+            {id: 40, title:"card 4", description:"4" },
+        ]
+    }
+
+    consoleTitle= (title)=> () =>{
+        console.log(title);
+    }
+    consoleItem = (id)=> ()=> {
+        const item=this.state.prodlist.find(item=>item.id===id)
+        console.log(item);
+    }
+
+    render() {
+        return (
+            <ul className='row product-list'>
+                {this.state.prodlist.map(item =>(
+                    <li key={item.id} className="col-3">
+                        <ProductItem title={item.title} 
+                        description={item.description}
+                        consoleTitle={this.consoleTitle(item.title)}
+                        consoleItem={this.consoleItem(item.id)}
+                        />
+                    </li>
+                ))}
+            </ul>
+        )}
 };
 
 export default ProductList;
